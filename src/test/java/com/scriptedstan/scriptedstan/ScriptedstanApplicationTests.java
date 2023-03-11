6ab6e510625896de468b1c2ccd9b7b9e21eee47f
Stanley <anukamstanley@gmail.com>
2023-03-11 16:20:52 -0500

    initail commit

---
+++ src/test/java/com/scriptedstan/scriptedstan/ScriptedstanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptedstan.scriptedstan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptedstanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
